f7b733fe5894722a4db42715abc74c2538023b75
mairitikk <mairi.tikk@gmail.com>
2025-12-02 12:51:33 +0100

    add theme

--- react-admin/src/theme.js
+++ react-admin/src/theme.js
@@ -201,8 +201,12 @@ export const ColorModeContext = createContext({
 
 export const useMode = () => {
   const [mode, setMode] = useState("dark");
-  const colorMode = useMemo(() => ({
-    toggleColorMode: () =>
-      setMode((prev) => (prev === "light" ? "dark" : "light")),
-  }));
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 };
