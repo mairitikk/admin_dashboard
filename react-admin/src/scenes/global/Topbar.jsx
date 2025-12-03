d3dbf255fd800418bb7ed8ca1b25c067f6d6a4f2
mairitikk <mairi.tikk@gmail.com>
2025-12-03 08:54:33 +0100

    add imports

--- react-admin/src/scenes/global/Topbar.jsx
+++ react-admin/src/scenes/global/Topbar.jsx
@@ -1,3 +1,15 @@
+import {Box, IconButton, useTheme} from "@mui/material";
+import {useContext} from "react";
+import {ColorModeContext, tokens} from "../../theme";
+import InputBase from "@mui/material";
+import LightModeOutlineIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlineIcon from "@mui/icons-material/LightModeOutlined"
+import NotificationsOutlineIcon from "@mui/icons-material/LightModeOutlined"
+import SettingsOutlineIcon from "@mui/icons-material/LightModeOutlined"
+import PersonOutlineIcon from "@mui/icons-material/LightModeOutlined"
+import SearchIcon from "@mui/icons-material/LightModeOutlined"
+
+
 const Topbar = () => {
   return (
     <div>Topbar</div>
