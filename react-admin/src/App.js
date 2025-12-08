e3faed40e6f128d1e1d5d1c96e0485394a131a0e
mairitikk <mairi.tikk@gmail.com>
2025-12-08 12:22:24 +0100

    add imports

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -3,6 +3,16 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
