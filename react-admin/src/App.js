005382c8e4aed6d31bf5b8a5de4a23cc7081b273
mairitikk <mairi.tikk@gmail.com>
2025-12-08 12:35:25 +0100

    add routes

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -3,16 +3,18 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Dashboard from "./scenes/dashboard";
-import Sidebar from "./scenes/global/Sidebar";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
-import Contacts from "./scenes/contacts";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
+
+//import Sidebar from "./scenes/global/Sidebar";
+//import Team from "./scenes/team";
+//import Invoices from "./scenes/invoices";
+//import Contacts from "./scenes/contacts";
+//import Bar from "./scenes/bar";
+//import Form from "./scenes/form";
+//import Line from "./scenes/line";
+//import Pie from "./scenes/pie";
+//import FAQ from "./scenes/faq";
+//import Geography from "./scenes/geography";
+//import Calender from "./scenes/calender";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -26,6 +28,22 @@ function App() {
             <Topbar></Topbar>
             <Routes>
               <Route path="/" element={<Dashboard></Dashboard>}></Route>
+              {/* 
+             <Route path="/sidebar" element={<Sidebar></Sidebar>}></Route>
+              <Route path="/team" element={<Team></Team>}></Route>
+              <Route path="/invoices" element={<Invoices></Invoices>}></Route>
+              <Route path="/contacts" element={<Contacts></Contacts>}></Route>
+              <Route path="/bar" element={<Bar></Bar>}></Route>
+              <Route path="/form" element={<Form></Form>}></Route>
+              <Route path="/line" element={<Line></Line>}></Route>
+              <Route path="/pie" element={<Pie></Pie>}></Route>
+              <Route path="/faq" element={<FAQ></FAQ>}></Route>
+              <Route
+                path="/geography"
+                element={<Geography></Geography>}
+              ></Route>
+              <Route path="/calender" element={<Calender></Calender>}>
+              </Route> */}
             </Routes>
           </main>
         </div>
